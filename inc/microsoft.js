1fa351628e42a4049041bda94a3dc3750d4f94bc
hancel.lin <imlinhanchao@foxmail.com>
2020-03-31 16:41:06 +0800

    更新 Micrsoft auto 判断逻辑

--- inc/microsoft.js
+++ inc/microsoft.js
@@ -11,8 +11,8 @@ async function translate(word, lang) {
             }
         });
         let token = req.body;
-        if (lang.from == 'auto') lang.from = null;
-        url = `https://api.cognitive.microsofttranslator.com/translate?${lang.from ? `from=${lang.from}&` : ''}to=${lang.to}&api-version=3.0&includeSentenceLength=false`
+        if (!lang.from) lang.from = 'AUTO';
+        url = `https://api.cognitive.microsofttranslator.com/translate?${lang.from}to=${lang.to}&api-version=3.0&includeSentenceLength=false`
         req = await got.post(url, {
             json: [ { Text: word } ],
             throwHttpErrors: false,
@@ -37,11 +37,20 @@ async function translate(word, lang) {
     }      
 }
 
-module.exports = async (word, { from, to }) => {
+module.exports = Object.assign(async (word, { from, to }) => {
     let lang = {
         from: from,
         to: to || 'zh'
     };
 
     return await translate(word, lang);
-};
+}, {
+    auto: '', 
+    af: 'af', ar: 'ar', bg: 'bg', bn: 'bn', bs: 'bs', ca: 'ca', cs: 'cs', cy: 'cy', da: 'da', 
+    de: 'de', el: 'el', en: 'en', es: 'es', et: 'et', fa: 'fa', fi: 'fi', fr: 'fr', he: 'he', 
+    hi: 'hi', hr: 'hr', ht: 'ht', hu: 'hu', id: 'id', is: 'is', it: 'it', ja: 'ja', ko: 'ko', 
+    lt: 'lt', lv: 'lv', ms: 'ms', mt: 'mt', mww: 'mww', nb: 'nb', nl: 'nl', pl: 'pl', pt: 'pt', 
+    ro: 'ro', ru: 'ru', sk: 'sk', sl: 'sl', 'sr-Latn': 'sr-Latn', sv: 'sv', sw: 'sw', ta: 'ta', 
+    th: 'th', 'tlh-Latn': 'tlh-Latn', tr: 'tr', uk: 'uk', ur: 'ur', vi: 'vi', 
+    'zh-Hans': 'zh-Hans', 'zh-Hant': 'zh-Hant', zh: 'zh', 'zh-TW': 'zh-TW', 'zh-CN': 'zh-CN'
+});
