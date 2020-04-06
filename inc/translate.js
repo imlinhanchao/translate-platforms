358bdf93398b94dcc481255228167d6e37c8c9b2
hancel.lin <imlinhanchao@foxmail.com>
2020-04-06 14:51:12 +0800

    添加腾讯翻译君接口

--- inc/translate.js
+++ inc/translate.js
@@ -2,8 +2,9 @@ const google = require('./google');
 const microsoft = require('./microsoft');
 const youdao = require('./youdao');
 const baidu = require('./baidu');
-const yandex = require('./yandex')
+const yandex = require('./yandex');
+const qq = require('./qq');
 
 module.exports = {
-    google, microsoft, youdao, baidu, yandex
+    google, microsoft, youdao, baidu, yandex, qq
 }
